e448833cc6574838e29283bae43ec5333f88d8f7
ckappgit <171122123+ckappgit@users.noreply.github.com>
2026-05-03 22:12:38 +0800

    feat:move prefernces ui

--- src/AkizipApplication.py
+++ src/AkizipApplication.py
@@ -69,52 +69,31 @@ class AkizipApplication(Adw.Application):
         window.set_modal(True)
         window.connect('close-request', self._on_preferences_closed)
 
-        page = Adw.PreferencesPage()
-        group = Adw.PreferencesGroup()
-        group.set_title(_('Compression'))
+        builder = Gtk.Builder.new_from_resource('/top/akizip/akizip/preferences.ui')
+        page = builder.get_object('page')
+        recommend_switch = builder.get_object('recommend_switch')
+        depth_spin = builder.get_object('depth_spin')
+        format_combo = builder.get_object('format_combo')
+        level_combo = builder.get_object('level_combo')
+        method_combo = builder.get_object('method_combo')
+        dictionary_spin = builder.get_object('dictionary_spin')
+        threads_spin = builder.get_object('threads_spin')
 
-        recommend_row = Adw.ActionRow()
-        recommend_row.set_title(_('Use compression recommendations'))
-        recommend_row.set_subtitle(_('Automatically choose 7-Zip parameters before compressing.'))
-        recommend_switch = Gtk.Switch()
-        recommend_switch.set_valign(Gtk.Align.CENTER)
         self.settings.bind(
             'use-compress-recommendation',
             recommend_switch,
             'active',
             Gio.SettingsBindFlags.DEFAULT,
         )
-        recommend_row.add_suffix(recommend_switch)
-        recommend_row.set_activatable_widget(recommend_switch)
 
-        depth_row = Adw.ActionRow()
-        depth_row.set_title(_('Scan depth'))
-        depth_row.set_subtitle(_('Maximum folder levels to scan for recommendations.'))
-        depth_spin = Gtk.SpinButton.new_with_range(0, 10, 1)
-        depth_spin.set_valign(Gtk.Align.CENTER)
         depth_spin.set_value(self.settings.get_int('compress-scan-depth'))
         depth_spin.connect(
             'value-changed',
             lambda spin: self.settings.set_int('compress-scan-depth', spin.get_value_as_int()),
         )
-        depth_row.add_suffix(depth_spin)
-        depth_row.set_activatable_widget(depth_spin)
 
-        group.add(recommend_row)
-        group.add(depth_row)
-
-        default_group = Adw.PreferencesGroup()
-        default_group.set_title(_('Default compression parameters'))
-        default_group.set_description(_('Used for normal compression and recommendation fallback.'))
-
-        format_row = Adw.ActionRow()
-        format_row.set_title(_('Format'))
-        format_combo = Gtk.ComboBoxText()
-        format_combo.append('7z', '7z')
-        format_combo.append('zip', 'zip')
         default_format = self._settings_get_string('default-compress-format', '7z')
         format_combo.set_active_id(default_format if default_format in ('7z', 'zip') else '7z')
-        format_combo.set_valign(Gtk.Align.CENTER)
         format_combo.connect(
             'changed',
             lambda combo: self._settings_set_string(
@@ -122,23 +101,9 @@ class AkizipApplication(Adw.Application):
                 combo.get_active_id() or '7z',
             ),
         )
-        format_row.add_suffix(format_combo)
-        format_row.set_activatable_widget(format_combo)
 
-        level_row = Adw.ActionRow()
-        level_row.set_title(_('Compression level'))
-        level_combo = Gtk.ComboBoxText()
-        for value, label in (
-                ('0', _('Store')),
-                ('1', _('Fastest')),
-                ('3', _('Fast')),
-                ('5', _('Normal')),
-                ('7', _('Maximum')),
-                ('9', _('Ultra'))):
-            level_combo.append(value, label)
         default_level = str(self._settings_get_int('default-compress-level', 5))
         level_combo.set_active_id(default_level if default_level in ('0', '1', '3', '5', '7', '9') else '5')
-        level_combo.set_valign(Gtk.Align.CENTER)
         level_combo.connect(
             'changed',
             lambda combo: self._settings_set_int(
@@ -146,21 +111,9 @@ class AkizipApplication(Adw.Application):
                 int(combo.get_active_id() or '5'),
             ),
         )
-        level_row.add_suffix(level_combo)
-        level_row.set_activatable_widget(level_combo)
 
-        method_row = Adw.ActionRow()
-        method_row.set_title(_('Algorithm'))
-        method_combo = Gtk.ComboBoxText()
-        for value, label in (
-                ('default', _('Default')),
-                ('LZMA', 'LZMA'),
-                ('PPMd', 'PPMd'),
-                ('BZip2', 'BZip2')):
-            method_combo.append(value, label)
         default_method = self._settings_get_string('default-compress-method', 'default')
         method_combo.set_active_id(default_method if default_method in ('default', 'LZMA', 'PPMd', 'BZip2') else 'default')
-        method_combo.set_valign(Gtk.Align.CENTER)
         method_combo.connect(
             'changed',
             lambda combo: self._settings_set_string(
@@ -168,14 +121,7 @@ class AkizipApplication(Adw.Application):
                 combo.get_active_id() or 'default',
             ),
         )
-        method_row.add_suffix(method_combo)
-        method_row.set_activatable_widget(method_combo)
 
-        dictionary_row = Adw.ActionRow()
-        dictionary_row.set_title(_('Dictionary size (MB)'))
-        dictionary_row.set_subtitle(_('0 uses the 7-Zip default.'))
-        dictionary_spin = Gtk.SpinButton.new_with_range(0, 1024, 1)
-        dictionary_spin.set_valign(Gtk.Align.CENTER)
         dictionary_spin.set_value(self._settings_get_int('default-compress-dictionary-size', 0))
         dictionary_spin.connect(
             'value-changed',
@@ -184,14 +130,7 @@ class AkizipApplication(Adw.Application):
                 spin.get_value_as_int(),
             ),
         )
-        dictionary_row.add_suffix(dictionary_spin)
-        dictionary_row.set_activatable_widget(dictionary_spin)
 
-        threads_row = Adw.ActionRow()
-        threads_row.set_title(_('Threads'))
-        threads_row.set_subtitle(_('0 uses the 7-Zip default.'))
-        threads_spin = Gtk.SpinButton.new_with_range(0, 64, 1)
-        threads_spin.set_valign(Gtk.Align.CENTER)
         threads_spin.set_value(self._settings_get_int('default-compress-threads', 0))
         threads_spin.connect(
             'value-changed',
@@ -200,17 +139,7 @@ class AkizipApplication(Adw.Application):
                 spin.get_value_as_int(),
             ),
         )
-        threads_row.add_suffix(threads_spin)
-        threads_row.set_activatable_widget(threads_spin)
 
-        default_group.add(format_row)
-        default_group.add(level_row)
-        default_group.add(method_row)
-        default_group.add(dictionary_row)
-        default_group.add(threads_row)
-
-        page.add(group)
-        page.add(default_group)
         window.add(page)
 
         self._preferences_window = window

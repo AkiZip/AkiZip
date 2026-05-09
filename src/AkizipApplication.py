c560eacd94d499c8d82e529803ab8fd56f1874ac
ckappgit <171122123+ckappgit@users.noreply.github.com>
2026-05-09 17:08:11 +0800

    version 0.2.2

--- src/AkizipApplication.py
+++ src/AkizipApplication.py
@@ -61,7 +61,7 @@ class AkizipApplication(Adw.Application):
         about = Adw.AboutDialog(application_name='Akizip',
                                 application_icon='top.akizip.akizip',
                                 developer_name='akizip',
-                                version='0.2.1',
+                                version='0.2.2',
                                 # Translators: Replace "translator-credits" with your name/username, and optionally an email or URL.
                                 translator_credits = _('translator-credits'),
                                 developers=['ckappgit','HungryNeko'],
